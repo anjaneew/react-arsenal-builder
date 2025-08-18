18fb88942e525b2a6574f15bb16dc83b526b4284
Anjanee Wijewardana <anjaneew@gmail.com>
2025-08-18 14:52:43 +0000

    fundamentals

---
+++ 01-greet-component/App.jsx
@@ -0,0 +1,10 @@
+import Greet from './components/Greet.jsx';
+//importing the component
+
+function App(){
+
+  //calling the component <Greet /> 
+  return <Greet />; //when single JSX element is returned, no brackets needed. and we add the ; at the end then
+}
+
+export default App;
---
+++ 01-greet-component/Greet.jsx
@@ -0,0 +1,19 @@
+// function Greet(){
+//     return(
+//         <>
+//         <h1>Hello Everyone</h1>
+//         </>
+//     );
+// }
+
+const Greet = () => {
+        return(
+        <>
+        <h1>Hello Everyone</h1>
+        </>
+    );
+}
+
+export default Greet;
+
+//registering the component 
