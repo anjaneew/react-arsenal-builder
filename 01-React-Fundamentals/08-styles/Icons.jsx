f79905a2dc5f3deea97a86625f7c5b9644498ed2
Anjanee Wijewardana <anjaneew@gmail.com>
2025-08-20 11:23:11 +0000

    Styles & Icons

---
+++ 01-React-Fundamentals/08-styles/Icons.jsx
@@ -0,0 +1,16 @@
+// import './MyApp.css';
+import { PiBirdBold } from "react-icons/pi";
+
+const MyApp = () => {
+
+    return(
+        <>
+        <div style={{fontSize: "50rem"}}>
+            <PiBirdBold />
+        </div>
+        
+        </>
+    );
+}
+
+export default MyApp;
